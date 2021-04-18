d7d239b721f2269902fdbefc60ff864bfb39c218
yanlifang <yanli@oregonstate.edu>
2021-04-18 16:23:26 -0500

    Login form created

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+var app = express();
+var path = require('path');
+var session = require('express-session');
+
+var publicRouter = require('./routes/public');
+
+app.get('/', (req, res) =>{
+    res.sendFile(__dirname+"/views/homepage.html");
+});
+
+app.use('/', publicRouter);
+
+
+
+var server = app.listen(3000, function(){
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log('App listening at http://%s:%s, host, port')
+})
