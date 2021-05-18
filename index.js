67f0b1441c2858f7447908fd289b6247eecceab1
yanlifang <yanli@oregonstate.edu>
2021-05-17 21:49:59 -0500

    correct redirect address

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.get('/google/callback',
     failureRedirect: '/fail', }),
     //successRedirect: '/profile'}));
     function(req, res) {
-      res.redirect('/https://naughty-hoover-9fe66f.netlify.app');
+      res.redirect('https://naughty-hoover-9fe66f.netlify.app');
     }
 );
 
@@ -86,7 +86,7 @@ app.get('/auth/linkedin',
 app.get('/auth/linkedin/callback', 
   passport.authenticate('linkedin', { 
       failureRedirect: '/fail',
-      successRedirect: '/https://naughty-hoover-9fe66f.netlify.app'
+      successRedirect: 'https://naughty-hoover-9fe66f.netlify.app'
 }));
 
 
